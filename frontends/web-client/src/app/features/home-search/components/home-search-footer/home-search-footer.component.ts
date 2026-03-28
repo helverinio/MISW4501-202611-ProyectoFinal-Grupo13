60c83275bc0ce5ae0c7f9161e62bbae7dc0c31f2
helverinio <h.wiesner@uniandes.edu.co>
2026-03-28 16:13:44 -0500

    feat: remove home search footer and related components from layout

--- frontends/web-client/src/app/features/home-search/components/home-search-footer/home-search-footer.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-home-search-footer',
-  templateUrl: './home-search-footer.component.html',
-  styleUrl: './home-search-footer.component.scss',
-})
-export class HomeSearchFooterComponent {}
--- frontends/web-client/src/app/features/home-search/pages/home-search-page/home-search-page.component.ts
+++ frontends/web-client/src/app/features/home-search/pages/home-search-page/home-search-page.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 
-import { HomeSearchFooterComponent } from '../../components/home-search-footer/home-search-footer.component';
-import { HomeSearchHeaderComponent } from '../../components/home-search-header/home-search-header.component';
 import { HomeSearchHeroSearchComponent } from '../../components/home-search-hero-search/home-search-hero-search.component';
-import { HomeSearchMobileNavigationComponent } from '../../components/home-search-mobile-navigation/home-search-mobile-navigation.component';
-import { HomeSearchMobileSelectorsComponent } from '../../components/home-search-mobile-selectors/home-search-mobile-selectors.component';
 import { HomeSearchPopularDestinationsComponent } from '../../components/home-search-popular-destinations/home-search-popular-destinations.component';
 import { HomeSearchRecentlyViewedComponent } from '../../components/home-search-recently-viewed/home-search-recently-viewed.component';
 import { HomeSearchTrustBadgesComponent } from '../../components/home-search-trust-badges/home-search-trust-badges.component';
@@ -12,14 +8,10 @@ import { HomeSearchTrustBadgesComponent } from '../../components/home-search-tru
 @Component({
   selector: 'app-home-search-page',
   imports: [
-    HomeSearchHeaderComponent,
-    HomeSearchMobileNavigationComponent,
     HomeSearchHeroSearchComponent,
     HomeSearchPopularDestinationsComponent,
     HomeSearchRecentlyViewedComponent,
     HomeSearchTrustBadgesComponent,
-    HomeSearchMobileSelectorsComponent,
-    HomeSearchFooterComponent,
   ],
   templateUrl: './home-search-page.component.html',
   styleUrl: './home-search-page.component.scss',
