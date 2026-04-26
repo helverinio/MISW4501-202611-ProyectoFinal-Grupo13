cd4540122b850697540c8974cffe4be844825ca5
Santiago Gómez Perdomo <s.gomezp2345@uniandes.edu.co>
2026-04-25 22:10:11 -0500

    email produccion

--- frontends/web-client/src/environments/environment.production.ts
+++ frontends/web-client/src/environments/environment.production.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true,
   apiBaseUrl: 'https://d1r8df79ch2otn.cloudfront.net/api/v1',
   emailJs: {
-    enabled: false,
+    enabled: true,
     endpoint: 'https://api.emailjs.com/api/v1.0/email/send',
-    serviceId: '',
-    templateId: '',
-    publicKey: '',
+    serviceId: 'service_ir1ojbq',
+    templateId: 'template_599obil',
+    publicKey: 'iqjzNhxcPVbGXAAo4',
   },
 };
