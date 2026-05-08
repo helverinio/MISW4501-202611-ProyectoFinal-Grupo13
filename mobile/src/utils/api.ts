ac066411776943867157bb694ae34f3cd7dca315
orlandoarnedo <orlandoarnedoc@gmail.com>
2026-05-08 10:31:36 -0500

    aim api utility to prod

--- mobile/src/utils/api.ts
+++ mobile/src/utils/api.ts
@@ -8,17 +8,17 @@ let backendUrl = '';
 const callerId = Constants.expoConfig?.extra?.REACT_APP_CALLER_ID || '';
 const extPaymentsBaseUrl =
   Constants.expoConfig?.extra?.REACT_APP_EXT_PAYMENTS_URL ||
-  'http://10.0.2.2:5001';
+  'https://d1r8df79ch2otn.cloudfront.net/ext-payments';
 console.log('ext-payments configured with URL:', extPaymentsBaseUrl);
 
 // Check environment - React Native uses different env variables than React web
 if (__DEV__) {
-  backendUrl = 'http://10.0.2.2:8081'//'http://10.0.2.2:8081'; // For Android emulator (nginx gateway)
+  backendUrl = 'https://d1r8df79ch2otn.cloudfront.net'//'http://10.0.2.2:8081'; // For Android emulator (nginx gateway)
 } else {
   // Production environment
   backendUrl =
     Constants.expoConfig?.extra?.REACT_APP_BACKEND_URL ||
-    'http://10.0.2.2:8081';
+    'https://d1r8df79ch2otn.cloudfront.net';
 }
 
 console.log('API configured with backend URL:', backendUrl);
