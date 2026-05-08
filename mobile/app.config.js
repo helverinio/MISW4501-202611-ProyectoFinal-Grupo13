59774afb1024852fdb9e346457a1554f85711781
orlandoarnedo <orlandoarnedoc@gmail.com>
2026-05-08 11:15:16 -0500

    mobile version bump

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: "travel-hub",
     slug: "travel-hub",
-    version: "0.2.1",
+    version: "0.3.1",
     orientation: "portrait",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
