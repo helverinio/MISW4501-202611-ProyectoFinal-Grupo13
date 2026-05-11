06b01bad1b75f04287b065474d61568c4ac9ac08
orlandoarnedo <orlandoarnedoc@gmail.com>
2026-05-11 13:11:46 -0500

    push notifications basic setup

--- mobile/src/_layout.tsx
+++ mobile/src/_layout.tsx
@@ -3,11 +3,15 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { I18nextProvider } from 'react-i18next';
 import i18n, { loadSavedLanguage } from './i18n';
+import { usePushNotifications } from './hooks/usePushNotifications';
 
 export default function RootLayout() {
   useEffect(() => {
     loadSavedLanguage();
   }, []);
+
+  // Initialize push notifications (registers token when user is logged in)
+  usePushNotifications();
 
   return (
     <I18nextProvider i18n={i18n}>
